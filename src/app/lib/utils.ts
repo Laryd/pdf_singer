83e45e7bd148c8a787b96012cd2cfc0e7d8d8cc3
Laryd <hillarymuboka@gmail.com>
2024-01-14 05:02:45 +0300

    fix: folder structure change

--- src/app/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs:ClassValue[]){
-    //will merge classes
-    return twMerge(clsx(inputs))
-}
