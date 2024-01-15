d4c557c17f0bd1c3847e7a709cbd285af4c65e43
Laryd <hillarymuboka@gmail.com>
2024-01-16 00:49:37 +0300

    feat: trpc auth-callback add

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,13 +1,19 @@
 import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client"
 
 
 const Page = () => {
-    const router = useRouter()
-    const searchParams = useSearchParams()
-    const origin = searchParams.get('origin')
-  return (
-    <div>page</div>
-  )
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined);
+  // Check for success directly in the component body
+  if (data?.success) {
+    // user is synced to the database
+    router.push(origin ? `/${origin}` : "/dashboard");
+  }
+  return <div>page</div>;
 }
 
 export default Page
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,10 +1,23 @@
-import {router} from "./trpc"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
 
-const appRouter = router({
+export const appRouter = router({
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-})
+    if (!user?.id || !user?.email)
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    //check if user is in the database
+
+    return {success: true}
+    
+  }),
+});
 
 //Export type router type signature
 //Not the router itself
 
-export type AppRouter = typeof appRouter
+export type AppRouter = typeof appRouter;
