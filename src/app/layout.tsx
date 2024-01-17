87d383b95d5b5c232e03f29bf975d8b61e71a685
Laryd <hillarymuboka@gmail.com>
2024-01-17 03:45:43 +0300

    feat:files list addd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "../lib/utils";
 import Navbar from "../components/Navbar";
 import Providers from "@/components/Providers";
+import "react-loading-skeleton/dist/skeleton.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
