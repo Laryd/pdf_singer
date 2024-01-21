41d34303df02113ac1aaaeeb4227225517e9cc7d
Laryd <hillarymuboka@gmail.com>
2024-01-22 02:54:40 +0300

    feat: pdf page navigation add

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,59 +1,127 @@
-"use client"
-import {Document, Page, pdfjs} from "react-pdf"
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import 'react-pdf/dist/Page/TextLayer.css'
+"use client";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 import { Button } from "./ui/button";
-import { ChevronDown, Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Input } from "./ui/input";
 import { useToast } from "./ui/use-toast";
-import {useResizeDetector} from 'react-resize-detector'
+import { useResizeDetector } from "react-resize-detector";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-
 interface PdfRendererProps {
-  url: string
+  url: string;
 }
-const PdfRenderer = ({url}: PdfRendererProps) => {
-  const {toast} = useToast()
-  const {width, ref} = useResizeDetector()
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const [numPages, setNumPages] = useState<number>();
+  const [currPage, setCurrPage] = useState<number>(1);
+  const { width, ref } = useResizeDetector();
+
+  const CustomPageValidator = z.object({
+    page: z
+      .string()
+      .refine((num) => Number(num) > 0 && Number(num) <= numPages!),
+  });
+
+  type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<TCustomPageValidator>({
+    defaultValues: {
+      page: "1",
+    },
+    resolver: zodResolver(CustomPageValidator),
+  });
+  const handlePageSubmit = ({page}: TCustomPageValidator) => {
+    setCurrPage(Number(page))
+    setValue("page", String(page))
+  }
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
-        <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between">
-            <div className="flex items-center gap-1 5">
-                <Button variant="ghost" aria-label="previous-page">
-                  <ChevronDown className="h-4 w-4"/>
-                </Button>
-                <div className="flex items-center gap-1.5">
-                  <Input className="w-12 h-8"/>
-                  <p className="text-zinc-700 text-sm space-x-1">
-                    <span>/</span>
-                    <span>5</span>
-                  </p>
-                </div>
-            </div>
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between">
+        <div className="flex items-center gap-1 5">
+          <Button
+            disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+              setValue("page", String(currPage))
+            }}
+            variant="ghost"
+            aria-label="previous-page"
+          >
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5">
+            <Input
+              {...register("page")}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit(handlePageSubmit)();
+                }
+              }}
+              className={cn("w-12 h-8", errors.page && "focus-visible:ring-red-500")}
+            />
+            <p className="text-zinc-700 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+          <Button
+            disabled={numPages === undefined || currPage === numPages}
+            onClick={() => {
+              setCurrPage((prev) =>
+                prev + 1 > numPages! ? numPages! : prev + 1
+              );
+               setValue("page", String(currPage));
+            }}
+            variant="ghost"
+            aria-label="next-page"
+          >
+            <ChevronUp className="h-4 w-4" />
+          </Button>
         </div>
-        <div className="flex-1 w-full max-h-screen">
-          <div>
-            <Document loading={
+        <div className="space-x-2">
+          
+        </div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div ref={ref}>
+          <Document
+            loading={
               <div>
-                <Loader2/>
+                <Loader2 className="h4 w-4 animate-spin" />
               </div>
-            } 
+            }
             onLoadError={() => {
               toast({
                 title: "Error loading PDF",
                 description: "Please try again later",
-                variant: "destructive"
-              })
+                variant: "destructive",
+              });
             }}
-            file={url} className='max-h-full'>
-              <Page pageNumber={1}/>
-
-            </Document>
-          </div>
+            onLoadSuccess={({ numPages }) => {
+              setNumPages(numPages);
+            }}
+            file={url}
+            className="max-h-full"
+          >
+            <Page width={width ? width : 1} pageNumber={currPage} />
+          </Document>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PdfRenderer
+export default PdfRenderer;
